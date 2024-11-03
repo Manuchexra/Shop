79c896d4c30830fe0de8e73f7fb6fa3d1d2c0871
Manuchehra <135632769+Manuchexra@users.noreply.github.com>
2024-11-03 16:55:28 +0500

    css

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -126,11 +126,12 @@ STATICFILES_DIRS = [
 
 
 
+
 STATICFILES = [os.path.join(BASE_DIR, 'staticfiles')]
 
 MEDIA_URL ='/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'templates/core/Delicious/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
